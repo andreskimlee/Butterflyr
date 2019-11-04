4180d5728c5982e1b7856a2c33291fd923c2e15a
Andres Kim <andreskimlee@gmail.com>
2019-11-04 17:07:57 -0500

    Finish edit bio box

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -30,7 +30,7 @@ class Api::UsersController < ApplicationController
     # end
   
     def user_params
-      params.require(:user).permit(:email, :password, :first_name, :last_name, :DOB, :gender, :prof_photo, :cover_photo, :cover_photo_url,  photos: [])
+      params.require(:user).permit(:bio, :email, :password, :first_name, :last_name, :DOB, :gender, :prof_photo, :cover_photo, :cover_photo_url,  photos: [])
     end
 end
   
